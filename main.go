4b391db8752810a5d344c2274030f99b741a496a
Pedro Tashima <pedrotashima42@gmail.com>
2024-02-19 07:55:57 -0300

    add releaser workflow

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+var version = "dev"
+
 func main() {
 	app := cli.App{
 		Name:                   "aves-curitiba",
 		Usage:                  "control and run the aves-curitiba scrapper",
 		UseShortOptionHandling: true,
+		Version:                version,
 		Commands:               []*cli.Command{cmd.DBCommand(), cmd.ScrapperCommand()},
 	}
 	if err := app.Run(os.Args); err != nil {
